a9cffd979ef47ab9ef78007a4963cd4656548989
rick <LinuxSuRen@users.noreply.github.com>
2024-06-18 05:50:51 +0000

    support to set the interval for watching

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Test
+Watch the listen ports, then send the payload to the specific webhook server.
+
+The webhook server should be a POST http server. Below is an example of the payload:
+
+```json
+{
+    "ports": [ 8080 ]
+}
+```
+
+## Limitation
+
+* `netstat` command is required
--- src/extension.ts
+++ src/extension.ts
@@ -11,20 +11,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "webhook" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('webhook.helloWorld', () => {
-    vscode.window.terminals.forEach((t) => {
-      t.processId.then((n) => {
-        vscode.window.showInformationMessage("process id " + n);
-      })
-    })
-
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from webhook!');
-
+  const interval = vscode.workspace.getConfiguration().get('webhook.interval', 2000)
+  setInterval(() => {
     getListeningPorts().then(ports => {
       const api = vscode.workspace.getConfiguration().get('webhook.address', '')
 
@@ -44,11 +32,25 @@ export function activate(context: vscode.ExtensionContext) {
 
       ports.forEach(p => {
         console.log(`get listen port: ${p}`);
-        vscode.window.showInformationMessage(`Listening port: ${p}`);
       })
     }).catch(error => {
       vscode.window.showInformationMessage(`Error: ${error.message}`);
     });
+  }, interval)
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('webhook.helloWorld', () => {
+    vscode.window.terminals.forEach((t) => {
+      t.processId.then((n) => {
+        vscode.window.showInformationMessage("process id " + n);
+      })
+    })
+
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from webhook!');
 	});
 
 	context.subscriptions.push(disposable);
